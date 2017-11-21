cac1ef9b9357935aeb2e49ca8ef0d383b510b5ff
Brad <bradleymckee@protonmail.com>
2017-11-20 20:32:15 -0500

    Commit
    here is some of my code.

---
+++ tree.cpp
@@ -0,0 +1,42 @@
+#include "tree.h"
+
+/*
+void  node::addChildNode(char childNode)
+{
+    if(childNode.compareTo(val)<0){
+
+    }
+}
+
+
+int node::returnValue(char x)
+{
+    return (int)x - 87;
+}
+
+
+void node::insert(node *&one,node *&two)
+{
+    if(one == NULL)
+    {
+        one = two;
+    }
+    else if(two -> val < one -> val)
+    {
+        insert(one ->left,two);
+    }
+    else
+        insert(one ->right,two);
+}
+
+
+
+
+void node::print()
+{
+
+    std::cout << "hi";
+}
+
+void tree::insertNode(char )
+*/
